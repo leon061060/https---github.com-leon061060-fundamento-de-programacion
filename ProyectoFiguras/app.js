4007365f1b81ce5d8af37cb191810a44a68846fc
leon061060 <leonhernandesleidy@gmail.com>
2025-06-07 19:04:42 -0500

    cambio5

--- ProyectoFiguras/app.js
+++ ProyectoFiguras/app.js
@@ -3,4 +3,7 @@ import {join,resolve} from 'path';
 import appRouter from "./routers/router.js";
 const app=express();
 const port=3000;
-app.listen(port3000)
+app.use("/",appRouter);
+app.listen(port, ()=>{
+    console.log('server running at http://localhost:${port}')
+})
